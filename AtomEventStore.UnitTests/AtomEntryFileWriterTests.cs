30972cd49ddd82d52b4ff4dfce39396d00a1b166
Mark Seemann <mark@seemann.ms>
2013-04-10 10:29:07 +0200

    Added AtomEntryFileWriter,
    which will be used to implement ISyndicationItemWriter by writing files in
    the Atom format.

---
+++ AtomEventStore.UnitTests/AtomEntryFileWriterTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit.Extensions;
+using Grean.AtomEventStore;
+using Xunit;
+
+namespace Grean.AtomEventStore.UnitTests
+{
+    public class AtomEntryFileWriterTests
+    {
+        [Theory, AutoAtomData]
+        public void SutIsSyndicationItemWriter(AtomEntryFileWriter sut)
+        {
+            Assert.IsAssignableFrom<ISyndicationItemWriter>(sut);
+        }
+    }
+}
