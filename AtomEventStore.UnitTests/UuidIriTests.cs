d28b5b5bff77ac3e53f1d52b1301adcad1423e9d
Mark Seemann <mark@seemann.ms>
2013-04-10 11:42:53 +0200

    Added UuidIri struct,
    intended to be a Value Object modeling a uuid IRI. The motivation is that
    I've already identifed three places in the code base where it refers to
    the hard-coded string "urn:uuid", which tells me that I can invoke the
    Rule of Three. I foresee more uuid parsing and matching in my immediate
    future.

---
+++ AtomEventStore.UnitTests/UuidIriTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ploeh.AutoFixture.Xunit;
+using Xunit;
+using Xunit.Extensions;
+using Grean.AtomEventStore;
+
+namespace Grean.AtomEventStore.UnitTests
+{
+    public class UuidIriTests
+    {
+        [Theory, AutoAtomData]
+        public void ToStringReturnsCorrectResult([Frozen]Guid g, UuidIri sut)
+        {
+            var actual = sut.ToString();
+            Assert.Equal("urn:uuid:" + g, actual);
+        }
+    }
+}
---
+++ AtomEventStore/UuidIri.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Grean.AtomEventStore
+{
+    public struct UuidIri
+    {
+        private readonly Guid id;
+
+        public UuidIri(Guid id)
+        {
+            this.id = id;
+        }
+
+        public override string ToString()
+        {
+            return "urn:uuid:" + this.id;
+        }
+    }
+}
