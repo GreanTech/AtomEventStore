4fee35339ef2425c40e9de87793765fa9e67d3bd
Mark Seemann <mark@seemann.ms>
2013-04-11 19:08:52 +0200

    Refactored production code,
    to (hopefully) make it more readable.

--- AtomEventStore/SyndicationStore.cs
+++ AtomEventStore/SyndicationStore.cs
@@ -28,45 +28,62 @@ namespace Grean.AtomEventStore
                 var changesetAddress =
                     new Uri(((Guid)changesetId).ToString(), UriKind.Relative);
 
-                var item = new SyndicationItem();
-                item.Id = changesetId.ToString();
-                item.Title = new TextSyndicationContent(
-                    "Changeset " + (Guid)changesetId);
-                item.Links.Add(
-                    new SyndicationLink
-                    {
-                        RelationshipType = "self",
-                        Uri = changesetAddress
-                    });
-                item.PublishDate = DateTimeOffset.Now;
-                item.LastUpdatedTime = item.PublishDate;
-                item.Authors.Add(new SyndicationPerson { Name = "Grean" });
-                item.Content = XmlSyndicationContent.CreateXmlContent(@event);
+                var item = CreateItem(@event, changesetId, changesetAddress);
                 this.entryWriter.Create(item);
 
-                var feedItem = item.Clone();
-                feedItem.Links.Clear();
-                feedItem.Links.Add(
-                    new SyndicationLink
-                    {
-                        RelationshipType = "via",
-                        Uri = changesetAddress
-                    });
-
-                var feed = new SyndicationFeed(new[] { feedItem });
-                feed.Id = id;
-                feed.Title = new TextSyndicationContent(
-                    "Head of event stream " + id);
-                feed.Links.Add(
-                    new SyndicationLink
-                    {
-                        RelationshipType = "self",
-                        Uri = new Uri(id, UriKind.Relative)
-                    });
-                feed.LastUpdatedTime = DateTimeOffset.Now;
-                feed.Authors.Add(new SyndicationPerson { Name = "Grean" });
-                this.headWriter.CreateOrUpdate(feed);
+                this.CreateOrUpdateHead(id, changesetAddress, item);
             });
+        }
+
+        private static SyndicationItem CreateItem(
+            object @event,
+            UuidIri changesetId,
+            Uri changesetAddress)
+        {
+            var item = new SyndicationItem();
+            item.Id = changesetId.ToString();
+            item.Title = new TextSyndicationContent(
+                "Changeset " + (Guid)changesetId);
+            item.Links.Add(
+                new SyndicationLink
+                {
+                    RelationshipType = "self",
+                    Uri = changesetAddress
+                });
+            item.PublishDate = DateTimeOffset.Now;
+            item.LastUpdatedTime = item.PublishDate;
+            item.Authors.Add(new SyndicationPerson { Name = "Grean" });
+            item.Content = XmlSyndicationContent.CreateXmlContent(@event);
+            return item;
+        }
+
+        private void CreateOrUpdateHead(
+            string id,
+            Uri changesetAddress,
+            SyndicationItem item)
+        {
+            var feedItem = item.Clone();
+            feedItem.Links.Clear();
+            feedItem.Links.Add(
+                new SyndicationLink
+                {
+                    RelationshipType = "via",
+                    Uri = changesetAddress
+                });
+
+            var feed = new SyndicationFeed(new[] { feedItem });
+            feed.Id = id;
+            feed.Title = new TextSyndicationContent(
+                "Head of event stream " + id);
+            feed.Links.Add(
+                new SyndicationLink
+                {
+                    RelationshipType = "self",
+                    Uri = new Uri(id, UriKind.Relative)
+                });
+            feed.LastUpdatedTime = DateTimeOffset.Now;
+            feed.Authors.Add(new SyndicationPerson { Name = "Grean" });
+            this.headWriter.CreateOrUpdate(feed);
         }
     }
 }
