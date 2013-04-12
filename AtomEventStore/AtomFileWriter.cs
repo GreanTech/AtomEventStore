26bbf22a04788b6903592f25759db4b1e92aa58f
Mark Seemann <mark@seemann.ms>
2013-04-12 11:50:32 +0200

    Implemented AtomFileWriter.
    Both method implementations have a cyclomatic complexity 'close to' 1 (it
    depends on how you measure the cyclomatic complexity of language
    constructs such as "using"), and since it involves out-of-process
    integration (with the file system) I think a Humble Object provides a
    better return on investment.
    Thus, no unit tests cover the two implementation methods.

--- AtomEventStore/AtomFileWriter.cs
+++ AtomEventStore/AtomFileWriter.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.ServiceModel.Syndication;
 using System.Text;
+using System.Xml;
 
 namespace Grean.AtomEventStore
 {
@@ -18,15 +19,29 @@ namespace Grean.AtomEventStore
 
         public void Create(SyndicationItem item)
         {
+            var fileName = this.CreateFileName(item.Links);
+            using (var w = XmlWriter.Create(fileName))
+                item.SaveAsAtom10(w);
         }
 
         public void CreateOrUpdate(SyndicationFeed feed)
         {
+            var fileName = this.CreateFileName(feed.Links);
+            using (var w = XmlWriter.Create(fileName))
+                feed.SaveAsAtom10(w);
         }
 
         public DirectoryInfo Directory
         {
             get { return this.directory; }
+        }
+
+        private string CreateFileName(IEnumerable<SyndicationLink> links)
+        {
+            var selfLink = links.Single(l => l.RelationshipType == "self");
+            return Path.Combine(
+                this.directory.ToString(),
+                selfLink.Uri.ToString());
         }
     }
 }
