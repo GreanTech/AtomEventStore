92f6d1f294f6047722c515d671ddfbd52ff4de0d
Mark Seemann <mark@seemann.ms>
2013-04-13 12:42:12 +0200

    Refactored to use id field and ignore id argument.

--- AtomEventStore/SyndicationStore.cs
+++ AtomEventStore/SyndicationStore.cs
@@ -35,10 +35,10 @@ namespace Grean.AtomEventStore
                     new Uri(((Guid)changesetId).ToString(), UriKind.Relative);
 
                 var item = CreateItem(@event, changesetId, changesetAddress);
-                this.AddPreviousLinkTo(item, id);
+                this.AddPreviousLinkTo(item);
 
                 this.entryWriter.Create(item);
-                this.CreateOrUpdateHead(id, changesetAddress, item);
+                this.CreateOrUpdateHead(changesetAddress, item);
             });
         }
 
@@ -64,9 +64,9 @@ namespace Grean.AtomEventStore
             return item;
         }
 
-        private void AddPreviousLinkTo(SyndicationItem item, string id)
+        private void AddPreviousLinkTo(SyndicationItem item)
         {
-            var head = this.headReader.Read(id);
+            var head = this.headReader.Read(this.id);
             var headEntry = head.Items.FirstOrDefault();
             if (headEntry != null)
             {
@@ -82,7 +82,6 @@ namespace Grean.AtomEventStore
         }
 
         private void CreateOrUpdateHead(
-            string id,
             Uri changesetAddress,
             SyndicationItem item)
         {
@@ -91,14 +90,14 @@ namespace Grean.AtomEventStore
                 l.RelationshipType = "via";
 
             var feed = new SyndicationFeed(new[] { feedItem });
-            feed.Id = id;
+            feed.Id = this.id;
             feed.Title = new TextSyndicationContent(
-                "Head of event stream " + id);
+                "Head of event stream " + this.id);
             feed.Links.Add(
                 new SyndicationLink
                 {
                     RelationshipType = "self",
-                    Uri = new Uri(id, UriKind.Relative)
+                    Uri = new Uri(this.id, UriKind.Relative)
                 });
             feed.LastUpdatedTime = DateTimeOffset.Now;
             feed.Authors.Add(new SyndicationPerson { Name = "Grean" });
