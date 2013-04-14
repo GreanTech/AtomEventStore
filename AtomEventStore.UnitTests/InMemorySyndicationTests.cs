7bf3f0a24f08c7b07cfc70c34b2ab0fd4a2bfed9
Mark Seemann <mark@seemann.ms>
2013-04-14 10:34:10 +0200

    Introduced InMemorySyndication,
    which will provide a fully functional implementation of all required
    SyndicationItem/Feed Reader/Writers by using an in-memory storage model.
    While this should be fully functional, it's also going to serve as an
    excellent Fake, which should make future testing of SyndicationStore
    easier.

---
+++ AtomEventStore.UnitTests/InMemorySyndicationTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Extensions;
+using Grean.AtomEventStore;
+
+namespace Grean.AtomEventStore.UnitTests
+{
+    public class InMemorySyndicationTests
+    {
+        [Theory, AutoAtomData]
+        public void SutIsSyndicationFeedReader(InMemorySyndication sut)
+        {
+            Assert.IsAssignableFrom<ISyndicationFeedReader>(sut);
+        }
+    }
+}
---
+++ AtomEventStore/InMemorySyndication.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel.Syndication;
+using System.Text;
+
+namespace Grean.AtomEventStore
+{
+    public class InMemorySyndication : ISyndicationFeedReader
+    {
+        public SyndicationFeed Read(string id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
