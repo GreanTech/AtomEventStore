c2116203f58f554b93f0ffdf9cf8e33e2622084d
Mark Seemann <mark@seemann.ms>
2013-04-16 21:38:10 +0200

    Introduced AtomAuthor.
    By the Atom specification, the Author element can have a couple of
    optional elements, so this Value Object may very likely gain one or more
    constructor overloads; the current test verifies initialization of the
    most modest constructor overload.

---
+++ AtomEventStore.UnitTests/AtomAuthorTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit.Extensions;
+using Grean.AtomEventStore;
+using Ploeh.AutoFixture.Xunit;
+using Xunit;
+
+namespace Grean.AtomEventStore.UnitTests
+{
+    public class AtomAuthorTests
+    {
+        [Theory, AutoAtomData]
+        public void NameIsCorrectWhenModestConstructorIsUsed(
+            [Frozen]string expected,
+            [Modest]AtomAuthor sut)
+        {
+            string actual = sut.Name;
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ AtomEventStore/AtomAuthor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Grean.AtomEventStore
+{
+    public class AtomAuthor
+    {
+        private readonly string name;
+
+        public AtomAuthor(string name)
+        {
+            this.name = name;
+        }
+
+        public string Name
+        {
+            get { return this.name; }
+        }
+    }
+}
