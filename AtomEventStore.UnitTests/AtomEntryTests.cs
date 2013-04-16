3eb72a7e98394f200e615aff9cccbeab9b3aa3fa
Mark Seemann <mark@seemann.ms>
2013-04-16 20:58:00 +0200

    Introduced the AtomEntry class.
    It will model an Atom entry, but will probably not end up implement the
    full Atom spec, but rather the parts of Atom required by AtomEventStore.

---
+++ AtomEventStore.UnitTests/AtomEntryTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Extensions;
+using Grean.AtomEventStore;
+using Ploeh.AutoFixture.Xunit;
+
+namespace Grean.AtomEventStore.UnitTests
+{
+    public class AtomEntryTests
+    {
+        [Theory, AutoAtomData]
+        public void IdIsCorrect([Frozen]UuidIri expected, AtomEntry sut)
+        {
+            UuidIri actual = sut.Id;
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ AtomEventStore/AtomEntry.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Grean.AtomEventStore
+{
+    public class AtomEntry
+    {
+        private readonly UuidIri id;
+
+        public AtomEntry(UuidIri id)
+        {
+            this.id = id;
+        }
+        
+        public UuidIri Id
+        {
+            get { return this.id; }
+        }
+    }
+}
