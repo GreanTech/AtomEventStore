c3ad7d70c3bfe77611a6763e77db9e286ee0c110
Mark Seemann <mark@seemann.ms>
2013-04-16 23:23:14 +0200

    Introduced AtomFeed.

---
+++ AtomEventStore.UnitTests/AtomFeedTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ploeh.AutoFixture.Xunit;
+using Xunit;
+using Xunit.Extensions;
+using Grean.AtomEventStore;
+
+namespace Grean.AtomEventStore.UnitTests
+{
+    public class AtomFeedTests
+    {
+        [Theory, AutoAtomData]
+        public void IdIsCorrect([Frozen]UuidIri expected, AtomFeed sut)
+        {
+            UuidIri actual = sut.Id;
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ AtomEventStore/AtomFeed.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Grean.AtomEventStore
+{
+    public class AtomFeed
+    {
+        private readonly UuidIri id;
+
+        public AtomFeed(UuidIri id)
+        {
+            this.id = id;
+        }
+
+        public UuidIri Id
+        {
+            get { return this.id; }
+        }
+    }
+}
