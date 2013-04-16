d75b5604f4c56eb0d3ff434ccd3d8b9e4243f8df
Mark Seemann <mark@seemann.ms>
2013-04-16 23:32:54 +0200

    Added AtomLink class,
    and Rel property. I debated whether to call it "RelationshipType" or just
    "Rel", but decided on "Rel" because it more closely matches Atom's "rel"
    attribute name.

---
+++ AtomEventStore.UnitTests/AtomLinkTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ploeh.AutoFixture.Xunit;
+using Xunit.Extensions;
+using Grean.AtomEventStore;
+using Xunit;
+
+namespace Grean.AtomEventStore.UnitTests
+{
+    public class AtomLinkTests
+    {
+        [Theory, AutoAtomData]
+        public void RelIsCorrect([Frozen]string expected, AtomLink sut)
+        {
+            string actual = sut.Rel;
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ AtomEventStore/AtomLink.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Grean.AtomEventStore
+{
+    public class AtomLink
+    {
+        private readonly string rel;
+
+        public AtomLink(string rel)
+        {
+            this.rel = rel;
+        }
+
+        public string Rel
+        {
+            get { return this.rel; }
+        }
+    }
+}
