224ecc82ce15481656430a5a53e2063bf29cda71
Mark Seemann <mark@seemann.ms>
2013-04-16 22:25:13 +0200

    Introduced XmlAtomContent,
    which will be used to model Atom content elements containing XML, which is
    what we need here. The Atom specification also allows other types of
    content, but if we ever need to model those, we'll need to introduce a
    polymorphic type here; until then: YAGNI.

---
+++ AtomEventStore.UnitTests/XmlAtomContentTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Grean.AtomEventStore;
+using Ploeh.AutoFixture.Xunit;
+using Xunit;
+using Xunit.Extensions;
+
+namespace Grean.AtomEventStore.UnitTests
+{
+    public class XmlAtomContentTests
+    {
+        [Theory, AutoAtomData]
+        public void ItemIsCorrect(
+            [Frozen]object expected,
+            XmlAtomContent sut)
+        {
+            var actual = sut.Item;
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ AtomEventStore/XmlAtomContent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Grean.AtomEventStore
+{
+    public class XmlAtomContent
+    {
+        private readonly object item;
+
+        public XmlAtomContent(object item)
+        {
+            this.item = item;
+        }
+
+        public object Item
+        {
+            get { return this.item; }
+        }
+    }
+}
