750c1649dcdd542b3934cb73249374cf840a7dc9
Mark Seemann <mark@seemann.ms>
2013-04-17 21:30:57 +0200

    Introduced SyndicationEventStream<T>.

---
+++ AtomEventStore.UnitTests/SyndicationEventStreamTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ploeh.AutoFixture.Xunit;
+using Xunit.Extensions;
+using Grean.AtomEventStore;
+using Xunit;
+
+namespace Grean.AtomEventStore.UnitTests
+{
+    public class SyndicationEventStreamTests
+    {
+        [Theory, AutoAtomData]
+        public void IdIsCorrect(
+            [Frozen]UuidIri expected,
+            SyndicationEventStream<TestEventX> sut)
+        {
+            UuidIri actual = sut.Id;
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ AtomEventStore/SyndicationEventStream.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Grean.AtomEventStore
+{
+    public class SyndicationEventStream<T>
+    {
+        private readonly UuidIri id;
+
+        public SyndicationEventStream(UuidIri id)
+        {
+            this.id = id;
+        }
+
+        public UuidIri Id
+        {
+            get { return this.id; }
+        }
+    }
+}
