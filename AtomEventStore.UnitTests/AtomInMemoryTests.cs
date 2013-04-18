ba6691fd6d059c5316ffcc26e64cc8a686fd6cf2
Mark Seemann <mark@seemann.ms>
2013-04-18 09:46:09 +0200

    Added AtomInMemory,
    which will be usable as an underlying 'store' for
    SyndicationEventStream<T>. I'll extract an interface from it when it's
    done. The current implementation is still naïve, so more tests will follow.

---
+++ AtomEventStore.UnitTests/AtomInMemoryTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Extensions;
+using Grean.AtomEventStore;
+
+namespace Grean.AtomEventStore.UnitTests
+{
+    public class AtomInMemoryTests
+    {
+        [Theory, AutoAtomData]
+        public void ClientCanReadWrittenEntry(
+            AtomInMemory sut,
+            AtomEntry entry,
+            TestEventX tex)
+        {
+            var expected = entry.WithContent(entry.Content.WithItem(tex));
+
+            using (var w = sut.CreateWriterFor(expected))
+                expected.WriteTo(w);
+            using (var r = sut.CreateReaderFor(expected.Id))
+            {
+                var actual = AtomEntry.ReadFrom(r);
+
+                Assert.Equal(expected, actual, new AtomEntryComparer());
+            }
+        }
+    }
+}
---
+++ AtomEventStore/AtomInMemory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+namespace Grean.AtomEventStore
+{
+    public class AtomInMemory
+    {
+        private StringBuilder entry;
+
+        public AtomInMemory()
+        {
+            this.entry = new StringBuilder();
+        }
+
+        public XmlWriter CreateWriterFor(AtomEntry atomEntry)
+        {
+            return XmlWriter.Create(entry);
+        }
+
+        public XmlReader CreateReaderFor(UuidIri id)
+        {
+            var sr = new StringReader(this.entry.ToString());
+            return XmlReader.Create(
+                sr,
+                new XmlReaderSettings { CloseInput = true });
+        }
+    }
+}
