38dd9271f35b14b3ceebccab93f3681161fe7910
Mark Seemann <mark@seemann.ms>
2013-04-18 21:55:14 +0200

    Implemented AtomEventsInFiles as a Humble Object.

--- AtomEventStore/AtomEventsInFiles.cs
+++ AtomEventStore/AtomEventsInFiles.cs
@@ -18,27 +18,62 @@ namespace Grean.AtomEventStore
 
         public XmlReader CreateEntryReaderFor(Uri href)
         {
-            throw new NotImplementedException();
+            var fileName = this.CreateFileName(href);
+            return XmlReader.Create(fileName);
         }
 
         public XmlWriter CreateEntryWriterFor(AtomEntry atomEntry)
         {
-            throw new NotImplementedException();
+            var fileName = this.CreateFileName(atomEntry.Links);
+            return XmlWriter.Create(fileName);
         }
 
         public XmlReader CreateFeedReaderFor(Uri href)
         {
-            throw new NotImplementedException();
+            var fileName = this.CreateFileName(href);
+
+            if (File.Exists(fileName))
+                return XmlReader.Create(fileName);
+
+            var id = new Guid(href.ToString());
+            var xml = new AtomFeed(
+                id,
+                "Index of event stream " + id,
+                DateTimeOffset.Now,
+                new AtomAuthor("Grean"),
+                Enumerable.Empty<AtomEntry>(),
+                new[]
+                {
+                    AtomEventStream.CreateSelfLinkFrom(id)
+                })
+                .ToXmlString();
+
+            var sr = new StringReader(xml);
+            return XmlReader.Create(
+                sr,
+                new XmlReaderSettings { CloseInput = true });
         }
 
         public XmlWriter CreateFeedWriterFor(AtomFeed atomFeed)
         {
-            throw new NotImplementedException();
+            var fileName = this.CreateFileName(atomFeed.Links);
+            return XmlWriter.Create(fileName);
         }
 
         public DirectoryInfo Directory
         {
             get { return this.directory; }
+        }
+
+        private string CreateFileName(IEnumerable<AtomLink> links)
+        {
+            var selfLink = links.Single(l => l.IsSelfLink);
+            return this.CreateFileName(selfLink.Href);
+        }
+
+        private string CreateFileName(Uri href)
+        {
+            return Path.Combine(this.directory.ToString(), href.ToString());
         }
     }
 }
