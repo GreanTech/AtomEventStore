148bfcd8db6fe238e5de2d65c5c7dc8103bc4838
Mark Seemann <mark@seemann.ms>
2013-04-18 11:38:29 +0200

    Renamed to AtomEventsInMemory,
    because the protocol is more specific than just storing Atom data in
    memory. The protocol specifies what happens if you attempt to write the
    same entry twice, or what happens if you attempt to read a feed that
    doesn't exist.

---
+++ AtomEventStore.UnitTests/AtomEventsInMemoryTests.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Extensions;
+using Grean.AtomEventStore;
+
+namespace Grean.AtomEventStore.UnitTests
+{
+    public class AtomEventsInMemoryTests
+    {
+        [Theory, AutoAtomData]
+        public void ClientCanReadWrittenEntry(
+            AtomEventsInMemory sut,
+            AtomEntryBuilder<TestEventX> entry)
+        {
+            var expected = entry.Build();
+
+            using (var w = sut.CreateEntryWriterFor(expected))
+                expected.WriteTo(w);
+            using (var r = sut.CreateEntryReaderFor(expected.Id))
+            {
+                var actual = AtomEntry.ReadFrom(r);
+
+                Assert.Equal(expected, actual, new AtomEntryComparer());
+            }
+        }
+
+        [Theory, AutoAtomData]
+        public void ClientCanReadFirstEntry(
+            AtomEventsInMemory sut,
+            AtomEntryBuilder<TestEventX> entry1,
+            AtomEntryBuilder<TestEventY> entry2)
+        {
+            var expected = entry1.Build();
+            var other = entry2.Build();
+
+            using (var w = sut.CreateEntryWriterFor(expected))
+                expected.WriteTo(w);
+            using (var w = sut.CreateEntryWriterFor(other))
+                other.WriteTo(w);
+
+            using (var r = sut.CreateEntryReaderFor(expected.Id))
+            {
+                var actual = AtomEntry.ReadFrom(r);
+
+                Assert.Equal(expected, actual, new AtomEntryComparer());
+            }
+        }
+
+        [Theory, AutoAtomData]
+        public void ClientCanReadSecondEntry(
+            AtomEventsInMemory sut,
+            AtomEntryBuilder<TestEventX> entry1,
+            AtomEntryBuilder<TestEventY> entry2)
+        {
+            var other = entry1.Build();
+            var expected = entry2.Build();
+
+            using (var w = sut.CreateEntryWriterFor(other))
+                other.WriteTo(w);
+            using (var w = sut.CreateEntryWriterFor(expected))
+                expected.WriteTo(w);
+
+            using (var r = sut.CreateEntryReaderFor(expected.Id))
+            {
+                var actual = AtomEntry.ReadFrom(r);
+
+                Assert.Equal(expected, actual, new AtomEntryComparer());
+            }
+        }
+
+        [Theory, AutoAtomData]
+        public void ClientCannotRepeatedWriteSameEntry(
+            AtomEventsInMemory sut,
+            AtomEntryBuilder<TestEventX> entryBuilder)
+        {
+            var entry = entryBuilder.Build();
+            using (var w = sut.CreateEntryWriterFor(entry))
+                entry.WriteTo(w);
+
+            Assert.Throws<InvalidOperationException>(
+                () => sut.CreateEntryWriterFor(entry));
+        }
+
+        [Theory, AutoAtomData]
+        public void ClientCanReadWrittenFeed(
+            AtomEventsInMemory sut,
+            AtomFeedBuilder<TestEventX> feedBuilder)
+        {
+            var expected = feedBuilder.Build();
+
+            using (var w = sut.CreateFeedWriterFor(expected))
+                expected.WriteTo(w);
+            using (var r = sut.CreateFeedReaderFor(expected.Id))
+            {
+                var actual = AtomFeed.ReadFrom(r);
+
+                Assert.Equal(expected, actual, new AtomFeedComparer());
+            }
+        }
+
+        [Theory, AutoAtomData]
+        public void ClientCanReadFirstFeed(
+            AtomEventsInMemory sut,
+            AtomFeedBuilder<TestEventX> feedBuilder1,
+            AtomFeedBuilder<TestEventY> feedBuilder2)
+        {
+            var expected = feedBuilder1.Build();
+            var other = feedBuilder2.Build();
+
+            using (var w = sut.CreateFeedWriterFor(expected))
+                expected.WriteTo(w);
+            using (var w = sut.CreateFeedWriterFor(other))
+                other.WriteTo(w);
+
+            using (var r = sut.CreateFeedReaderFor(expected.Id))
+            {
+                var actual = AtomFeed.ReadFrom(r);
+
+                Assert.Equal(expected, actual, new AtomFeedComparer());
+            }
+        }
+
+        [Theory, AutoAtomData]
+        public void ClientCanReadSecondFeed(
+            AtomEventsInMemory sut,
+            AtomFeedBuilder<TestEventX> feedBuilder1,
+            AtomFeedBuilder<TestEventY> feedBuilder2)
+        {
+            var other = feedBuilder1.Build();
+            var expected = feedBuilder2.Build();
+
+            using (var w = sut.CreateFeedWriterFor(other))
+                other.WriteTo(w);
+            using (var w = sut.CreateFeedWriterFor(expected))
+                expected.WriteTo(w);
+
+            using (var r = sut.CreateFeedReaderFor(expected.Id))
+            {
+                var actual = AtomFeed.ReadFrom(r);
+
+                Assert.Equal(expected, actual, new AtomFeedComparer());
+            }
+        }
+    }
+}
