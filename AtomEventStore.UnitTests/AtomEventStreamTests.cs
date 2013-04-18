602e7b6796a70ded2b747f0971f915e87dd53fea
Mark Seemann <mark@seemann.ms>
2013-04-18 17:52:08 +0200

    Refactored test to use a Likeness for AtomFeed too.

--- AtomEventStore.UnitTests/AtomEventStreamTests.cs
+++ AtomEventStore.UnitTests/AtomEventStreamTests.cs
@@ -57,7 +57,10 @@ namespace Grean.AtomEventStore.UnitTests
 
             // Verify outcome
             var writtenFeed = storage.Feeds.Select(AtomFeed.Parse).Single();
-            AssertFeed(before, sut.Id, expectedEvent, writtenFeed);
+            var expectedFeed = new AtomFeedLikeness(before, sut.Id, expectedEvent);
+            Assert.True(
+                expectedFeed.Equals(writtenFeed),
+                "Expected feed must match actual feed.");
 
             var writtenEntry = storage.Entries.Select(AtomEntry.Parse).Single();
             var expectedEntry = new AtomEntryLikeness(
@@ -74,33 +77,57 @@ namespace Grean.AtomEventStore.UnitTests
             // Teardown
         }
 
-        private static void AssertFeed(
-            DateTimeOffset before,
-            UuidIri expectedId,
-            object expectedEvent,
-            AtomFeed actual)
+        private class AtomFeedLikeness
         {
-            var expectedEntry = new AtomEntryLikeness(
-                before,
-                expectedEvent, 
-                new[]
-                {
-                    AtomEventStream
-                        .CreateSelfLinkFrom(
-                            actual.Entries.Single().Id)
-                        .ToViaLink()
-                });
+            private readonly DateTimeOffset minimumTime;
+            private readonly UuidIri expectedId;
+            private readonly object expectedEvent;
 
-            Assert.Equal(expectedId, actual.Id);
-            Assert.Equal("Head of event stream " + (Guid)expectedId, actual.Title);
-            Assert.True(before <= actual.Updated, "Updated should be very recent.");
-            Assert.True(actual.Updated <= DateTimeOffset.Now, "Updated should not be in the future.");
-            Assert.True(
-                expectedEntry.Equals(actual.Entries.Single()),
-                "Expected entry must match actual entry.");
-            Assert.Contains(
-                AtomEventStream.CreateSelfLinkFrom(expectedId),
-                actual.Links);
+            public AtomFeedLikeness(
+                DateTimeOffset minimumTime,
+                UuidIri expectedId,
+                object expectedEvent)
+            {
+                this.minimumTime = minimumTime;
+                this.expectedId = expectedId;
+                this.expectedEvent = expectedEvent;
+            }
+
+            public override bool Equals(object obj)
+            {
+                var actual = obj as AtomFeed;
+                if (actual == null)
+                    return base.Equals(obj);
+
+                var expectedEntry = new AtomEntryLikeness(
+                    this.minimumTime,
+                    this.expectedEvent,
+                    new[]
+                    {
+                        AtomEventStream
+                            .CreateSelfLinkFrom(
+                                actual.Entries.Single().Id)
+                            .ToViaLink()
+                    });
+
+                Assert.Equal(this.expectedId, actual.Id);
+                Assert.Equal("Head of event stream " + (Guid)this.expectedId, actual.Title);
+                Assert.True(this.minimumTime <= actual.Updated, "Updated should be very recent.");
+                Assert.True(actual.Updated <= DateTimeOffset.Now, "Updated should not be in the future.");
+                Assert.True(
+                    expectedEntry.Equals(actual.Entries.Single()),
+                    "Expected entry must match actual entry.");
+                Assert.Contains(
+                    AtomEventStream.CreateSelfLinkFrom(this.expectedId),
+                    actual.Links);
+
+                return true;
+            }
+
+            public override int GetHashCode()
+            {
+                return 0;
+            }
         }
 
         private class AtomEntryLikeness
