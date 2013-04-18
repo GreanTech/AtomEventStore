5da9dd6c9b83dbceaf3699c86a61dd7a52ed58ec
Mark Seemann <mark@seemann.ms>
2013-04-18 16:33:32 +0200

    Appended first event to event stream.

--- AtomEventStore.UnitTests/AtomEventStreamTests.cs
+++ AtomEventStore.UnitTests/AtomEventStreamTests.cs
@@ -29,5 +29,85 @@ namespace Grean.AtomEventStore.UnitTests
             IAtomEventStorage actual = sut.Storage;
             Assert.Equal(expected, actual);
         }
+
+        [Theory, AutoAtomData]
+        public void AppendAsyncCorrectlyStoresFeedAndEntry(
+            [Frozen(As = typeof(IAtomEventStorage))]AtomEventsInMemory storage,
+            AtomEventStream<TestEventX> sut,
+            TestEventX expectedEvent)
+        {
+            // Fixture setup
+            var before = DateTimeOffset.Now;
+
+            // Exercise system
+            sut.AppendAsync(expectedEvent).Wait();
+
+            // Verify outcome
+            var writtenFeed = storage.Feeds.Select(AtomFeed.Parse).Single();
+            AssertFeed(before, sut.Id, expectedEvent, writtenFeed);
+
+            var writtenEntry = storage.Entries.Select(AtomEntry.Parse).Single();
+            AssertAtomEntry(
+                before,
+                expectedEvent,
+                new[]
+                {
+                    AtomLink.CreateSelfLink(
+                        new Uri(
+                            ((Guid)writtenEntry.Id).ToString(),
+                            UriKind.Relative))
+                },
+                writtenEntry);
+
+            // Teardown
+        }
+
+        private static void AssertFeed(
+            DateTimeOffset before,
+            UuidIri expectedId,
+            object expectedEvent,
+            AtomFeed actual)
+        {
+            Assert.Equal(expectedId, actual.Id);
+            Assert.Equal("Head of event stream " + (Guid)expectedId, actual.Title);
+            Assert.True(before <= actual.Updated, "Updated should be very recent.");
+            Assert.True(actual.Updated <= DateTimeOffset.Now, "Updated should not be in the future.");
+            AssertAtomEntry(
+                before,
+                expectedEvent,
+                new[]
+                {
+                    new AtomLink(
+                        "via",
+                        new Uri(
+                            ((Guid)actual.Entries.Single().Id).ToString(),
+                            UriKind.Relative))
+                },
+                actual.Entries.Single());
+            Assert.Contains(
+                AtomLink.CreateSelfLink(
+                    new Uri(
+                        ((Guid)expectedId).ToString(),
+                        UriKind.Relative)),
+                actual.Links);
+        }
+
+        private static void AssertAtomEntry(
+            DateTimeOffset before,
+            object expectedEvent,
+            IEnumerable<AtomLink> expectedLinks,
+            AtomEntry actual)
+        {
+            Assert.NotEqual(default(UuidIri), actual.Id);
+            Assert.Equal("Changeset " + (Guid)actual.Id, actual.Title);
+            Assert.True(before <= actual.Published, "Published should be very recent.");
+            Assert.True(actual.Published <= DateTimeOffset.Now, "Published should not be in the future.");
+            Assert.True(before <= actual.Updated, "Updated should be very recent.");
+            Assert.True(actual.Updated <= DateTimeOffset.Now, "Updated should not be in the future.");
+            Assert.True(
+                new HashSet<AtomLink>(expectedLinks).IsSubsetOf(actual.Links),
+                "Expected links must be contained in actual links.");
+            Assert.Equal(expectedEvent, actual.Content.Item);
+        }
     }
 }
--- AtomEventStore/AtomEventStream.cs
+++ AtomEventStore/AtomEventStream.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Grean.AtomEventStore
 {
@@ -18,6 +19,44 @@ namespace Grean.AtomEventStore
             this.storage = storage;
         }
 
+        public Task AppendAsync(T @event)
+        {
+            return Task.Factory.StartNew(() =>
+            {
+                var now = DateTimeOffset.Now;
+
+                var changesetId = UuidIri.NewId();
+                var entry = new AtomEntry(
+                    changesetId,
+                    "Changeset " + (Guid)changesetId,
+                    now,
+                    now,
+                    new AtomAuthor("Grean"),
+                    new XmlAtomContent(@event),
+                    new[] { CreateSelfLinkFrom(changesetId) });
+
+                var feed = new AtomFeed(
+                    this.id,
+                    "Head of event stream " + (Guid)this.id,
+                    now,
+                    new AtomAuthor("Grean"),
+                    new[] { entry.WithLinks(entry.Links.Select(ChangeRelFromSelfToVia)) },
+                    new[] { CreateSelfLinkFrom(this.id) });
+
+                using (var w = this.storage.CreateFeedWriterFor(feed))
+                    feed.WriteTo(w);
+                using (var w = this.storage.CreateEntryWriterFor(entry))
+                    entry.WriteTo(w);
+            });
+        }
+
+        private static AtomLink ChangeRelFromSelfToVia(AtomLink link)
+        {
+            if (link.IsSelfLink)
+                return link.WithRel("via");
+            return link;
+        }
+
         public UuidIri Id
         {
             get { return this.id; }
@@ -26,6 +65,14 @@ namespace Grean.AtomEventStore
         public IAtomEventStorage Storage
         {
             get { return this.storage; }
+        }
+
+        private static AtomLink CreateSelfLinkFrom(UuidIri id)
+        {
+            return AtomLink.CreateSelfLink(
+                new Uri(
+                    ((Guid)id).ToString(),
+                    UriKind.Relative));
         }
     }
 }
