bb403069b43b43e6dfda4ba295bac35c22adaca5
Mark Seemann <mark@ploeh.dk>
2013-05-01 14:07:37 +0200

    Added AtomEventsOnAzure class,
    which will implement the Azure Blob Storage-specifics for AtomEventStore.
    Due to the nature of this class, the only way to test it would be against
    the Azure Blob Storage itself, which would indicate a slow and fragile set
    of integration tests. It's my hope that the implementation is going to be
    simple enough that we can treat it as a Humble Object, so that's what I'll
    do until proven wrong about that assumption.

---
+++ AtomEventStore.AzureBlob/AtomEventsOnAzure.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Grean.AtomEventStore.AzureBlob
+{
+    public class AtomEventsOnAzure : IAtomEventStorage
+    {
+        public XmlReader CreateEntryReaderFor(Uri href)
+        {
+            throw new NotImplementedException();
+        }
+
+        public XmlWriter CreateEntryWriterFor(AtomEntry atomEntry)
+        {
+            throw new NotImplementedException();
+        }
+
+        public XmlReader CreateFeedReaderFor(Uri href)
+        {
+            throw new NotImplementedException();
+        }
+
+        public XmlWriter CreateFeedWriterFor(AtomFeed atomFeed)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
