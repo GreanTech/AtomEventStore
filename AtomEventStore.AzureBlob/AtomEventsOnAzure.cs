8cf00203abc8e33f801ce398897e7ef22fbe806f
Mark Seemann <mark@ploeh.dk>
2013-05-01 14:44:13 +0200

    First implementation of AtomEventsOnAzure.

--- AtomEventStore.AzureBlob/AtomEventsOnAzure.cs
+++ AtomEventStore.AzureBlob/AtomEventsOnAzure.cs
@@ -1,32 +1,83 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml;
+using Microsoft.WindowsAzure.Storage.Blob;
 
 namespace Grean.AtomEventStore.AzureBlob
 {
     public class AtomEventsOnAzure : IAtomEventStorage
     {
+        private readonly CloudBlobContainer container;
+
+        public AtomEventsOnAzure(CloudBlobContainer container)
+        {
+            this.container = container;
+        }
+
         public XmlReader CreateEntryReaderFor(Uri href)
         {
-            throw new NotImplementedException();
+            var blobRef = this.CreateBlobReference(href);
+            return XmlReader.Create(
+                blobRef.OpenRead(),
+                new XmlReaderSettings { CloseInput = true });
         }
 
         public XmlWriter CreateEntryWriterFor(AtomEntry atomEntry)
         {
-            throw new NotImplementedException();
+            var blobRef = this.CreateBlobReference(atomEntry.Links);            
+            return XmlWriter.Create(
+                blobRef.OpenWrite(),
+                new XmlWriterSettings { CloseOutput = true });
         }
 
         public XmlReader CreateFeedReaderFor(Uri href)
         {
-            throw new NotImplementedException();
+            var blobRef = this.CreateBlobReference(href);
+            if (blobRef.Exists())
+                return XmlReader.Create(
+                    blobRef.OpenRead(),
+                    new XmlReaderSettings { CloseInput = true });
+
+            var id = new Guid(href.ToString());
+            var xml = new AtomFeed(
+                id,
+                "Index of event stream " + id,
+                DateTimeOffset.Now,
+                new AtomAuthor("Grean"),
+                Enumerable.Empty<AtomEntry>(),
+                new[]
+                {
+                    AtomEventStream.CreateSelfLinkFrom(id)
+                })
+                .ToXmlString();
+
+            var sr = new StringReader(xml);
+            return XmlReader.Create(
+                sr,
+                new XmlReaderSettings { CloseInput = true });
         }
 
         public XmlWriter CreateFeedWriterFor(AtomFeed atomFeed)
         {
-            throw new NotImplementedException();
+            var blobRef = this.CreateBlobReference(atomFeed.Links);
+            return XmlWriter.Create(
+                blobRef.OpenWrite(),
+                new XmlWriterSettings { CloseOutput = true });
+        }
+
+        private CloudBlockBlob CreateBlobReference(IEnumerable<AtomLink> links)
+        {
+            var selfLink = links.Single(l => l.IsSelfLink);
+            return this.CreateBlobReference(selfLink.Href);
+        }
+
+        private CloudBlockBlob CreateBlobReference(Uri href)
+        {
+            return this.container.GetBlockBlobReference(href.ToString());
         }
     }
 }
