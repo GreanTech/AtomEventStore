82261822f9f896ff9848a8e942d1e9b9ef699c48
Mark Seemann <mark@ploeh.dk>
2013-05-10 13:50:29 +0200

    Added an .msbuild file to make a release build.
    This can be called from either build-release.sh (if you're in Bash) or
    Build-Release.ps1 (if you're in PowerShell).
    
    In the future, I'd like to add more steps to this build file, including
    the building (and publishing of NuGet packages), but also running tests
    first.

--- Build-Release.ps1
+++ Build-Release.ps1
@@ -1,3 +1,3 @@
-.$env:windir\Microsoft.NET\Framework\v4.0.30319\MSBuild AtomEventStore.AzureBlob.sln /property:Configuration=Release
+.$env:windir\Microsoft.NET\Framework\v4.0.30319\MSBuild BuildRelease.msbuild /property:Configuration=Release
 nuget pack AtomEventStore.nuspec -prop Configuration=Release
 nuget pack AtomEventStore.AzureBlob.nuspec -prop Configuration=Release
--- build-release.sh
+++ build-release.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-$WINDIR/Microsoft.NET/Framework/v4.0.30319/MSBuild.exe AtomEventStore.AzureBlob.sln /property:Configuration=Release
+$WINDIR/Microsoft.NET/Framework/v4.0.30319/MSBuild.exe BuildRelease.msbuild /property:Configuration=Release
 nuget pack AtomEventStore.nuspec -prop Configuration=Release
 nuget pack AtomEventStore.AzureBlob.nuspec -prop Configuration=Release
