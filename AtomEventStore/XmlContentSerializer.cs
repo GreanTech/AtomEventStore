67493e7c49ac60d4c926593628169b5e3fd02fcc
Mark Seemann <mark@ploeh.dk>
2013-12-12 19:36:20 +0100

    Introduced XmlContentSerializer,
    which will be used to serialize and deserialize types using
    System.Xml.Serialization.XmlSerializer.

---
+++ AtomEventStore/XmlContentSerializer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+namespace Grean.AtomEventStore
+{
+    public class XmlContentSerializer : IContentSerializer
+    {
+        public void Serialize(XmlWriter xmlWriter, object value)
+        {
+            throw new ArgumentNullException("xmlWriter");
+        }
+
+        public XmlAtomContent Deserialize(XmlReader xmlReader)
+        {
+            throw new ArgumentNullException("xmlReader");
+        }
+    }
+}
