fcc154d60f90b2da9b27804dd5485c4d6f376ebf
Mark Seemann <mark@ploeh.dk>
2014-02-06 10:17:06 +0100

    Introduced AtomEventObserver<T>,
    which will replace the write responsibilities of AtomEventStream<T>. It
    seems more reasonable that the writer of events is a separate class,
    because there's going to be at least two Iterators: A Forward traversal
    Iterator, and a Backwards traversal Iterator.

---
+++ AtomEventStore.UnitTests/AtomEventObserverTests.cs
@@ -0,0 +1,21 @@
+﻿using Ploeh.AutoFixture.Idioms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit.Extensions;
+using Grean.AtomEventStore;
+
+namespace Grean.AtomEventStore.UnitTests
+{
+    public class AtomEventObserverTests
+    {
+        [Theory, AutoAtomData]
+        public void PropertiesAreCorrectlyInitialized(
+            ConstructorInitializedMemberAssertion assertion)
+        {
+            assertion.Verify(typeof(AtomEventObserver<TestEventX>));
+        }
+    }
+}
