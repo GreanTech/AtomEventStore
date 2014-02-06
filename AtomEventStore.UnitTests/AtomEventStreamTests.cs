9b970922dc68c8369e88063ca49f1e473e0391c8
Mark Seemann <mark@ploeh.dk>
2014-02-06 10:41:40 +0100

    Extracted AtomEntryLikeness to a public class,
    since I'll soon need to use it from other test classes too.

--- AtomEventStore.UnitTests/AtomEventStreamTests.cs
+++ AtomEventStore.UnitTests/AtomEventStreamTests.cs
@@ -414,40 +414,6 @@ namespace Grean.AtomEventStore.UnitTests
             }
         }
 
-        private class AtomEntryLikeness
-        {
-            private readonly DateTimeOffset minimumTime;
-            private readonly object expectedEvent;
-
-            public AtomEntryLikeness(
-                DateTimeOffset minimumTime,
-                object expectedEvent)
-            {
-                this.minimumTime = minimumTime;
-                this.expectedEvent = expectedEvent;
-            }
-
-            public override bool Equals(object obj)
-            {
-                var actual = obj as AtomEntry;
-                if (actual == null)
-                    return base.Equals(obj);
-
-                return !object.Equals(default(UuidIri), actual.Id)
-                    && object.Equals("Changeset " + (Guid)actual.Id, actual.Title)
-                    && this.minimumTime <= actual.Published
-                    && actual.Published <= DateTimeOffset.Now
-                    && this.minimumTime <= actual.Updated
-                    && actual.Updated <= DateTimeOffset.Now
-                    && object.Equals(this.expectedEvent, actual.Content.Item);
-            }
-
-            public override int GetHashCode()
-            {
-                return 0;
-            }
-        }
-
         [Theory, AutoAtomData]
         public void SutIsEnumerable(AtomEventStream<TestEventY> sut)
         {
