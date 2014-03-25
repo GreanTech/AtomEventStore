e9756781d12cdd53297bd5b5ec07e78ebb0a8b30
Mark Seemann <mark@ploeh.dk>
2014-03-25 08:23:53 +0100

    Introduced FifoEvents<T>,
    which will replace AtomEventStream<T>, supplying First In, First Out
    enumeration of events in the underlying store.
    
    The motivation for replacing AtomEventStream<T> is to decouple readers
    from writers, and to make the traversal direction more explicit. In
    addtion to FifoEvents<T>, we should also add the reverse Iterator.

---
+++ AtomEventStore.UnitTests/FifoEventsTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Extensions;
+using Grean.AtomEventStore;
+
+namespace Grean.AtomEventStore.UnitTests
+{
+    public class FifoEventsTests
+    {
+        [Theory, AutoAtomData]
+        public void SutIsEnumerable(FifoEvents<XmlAttributedTestEventX> sut)
+        {
+            Assert.IsAssignableFrom<IEnumerable<XmlAttributedTestEventX>>(sut);
+        }
+    }
+}
---
+++ AtomEventStore/FifoEvents.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Grean.AtomEventStore
+{
+    public class FifoEvents<T> : IEnumerable<T>
+    {
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
