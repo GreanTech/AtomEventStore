569f3a0643006f88719f47c3fc323fb99c0560b0
Mark Seemann <mark@ploeh.dk>
2014-03-26 22:11:05 +0100

    Wrote XML to writer using DataContractSerializer.

--- AtomEventStore.UnitTests/DataContractContentSerializerTests.cs
+++ AtomEventStore.UnitTests/DataContractContentSerializerTests.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using Xunit.Extensions;
 using Grean.AtomEventStore;
 using Xunit;
+using System.Xml;
+using System.Xml.Linq;
 
 namespace Grean.AtomEventStore.UnitTests
 {
@@ -15,6 +17,27 @@ namespace Grean.AtomEventStore.UnitTests
         public void SutIsContentSerializer(DataContractContentSerializer sut)
         {
             Assert.IsAssignableFrom<IContentSerializer>(sut);
+        }
+
+        [Theory, AutoAtomData]
+        public void SerializeCorrectlySerializesAttributedClassInstance(
+            DataContractContentSerializer sut,
+            DataContractTestEventX xate)
+        {
+            var sb = new StringBuilder();
+            using (var w = XmlWriter.Create(sb))
+            {
+                sut.Serialize(w, xate);
+                w.Flush();
+                var actual = sb.ToString();
+
+                var expected = XDocument.Parse(
+                    "<test-event-x xmlns:i=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns=\"http://grean.rocks/dc\">" +
+                    "  <number>" + xate.Number + "</number>" +
+                    "  <text>" + xate.Text + "</text>" +
+                    "</test-event-x>");
+                Assert.Equal(expected, XDocument.Parse(actual), new XNodeEqualityComparer());
+            }
         }
     }
 }
--- AtomEventStore/DataContractContentSerializer.cs
+++ AtomEventStore/DataContractContentSerializer.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Xml;
 
@@ -14,6 +15,9 @@ namespace Grean.AtomEventStore
                 throw new ArgumentNullException("xmlWriter");
             if (value == null)
                 throw new ArgumentNullException("value");
+
+            var serializer = new DataContractSerializer(value.GetType());
+            serializer.WriteObject(xmlWriter, value);
         }
 
         public XmlAtomContent Deserialize(XmlReader xmlReader)
