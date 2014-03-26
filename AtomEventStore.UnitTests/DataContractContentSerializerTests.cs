e415c066a54494a1571f69c42a5731403a6cf796
Mark Seemann <mark@ploeh.dk>
2014-03-26 22:00:14 +0100

    Introduced DataContractContentSerializer,
    which will use a DataContractSerializer to serialize and deserialize
    objects, just like the XmlContentSerializer uses XmlSerializer.

---
+++ AtomEventStore.UnitTests/DataContractContentSerializerTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit.Extensions;
+using Grean.AtomEventStore;
+using Xunit;
+
+namespace Grean.AtomEventStore.UnitTests
+{
+    public class DataContractContentSerializerTests
+    {
+        [Theory, AutoAtomData]
+        public void SutIsContentSerializer(DataContractContentSerializer sut)
+        {
+            Assert.IsAssignableFrom<IContentSerializer>(sut);
+        }
+    }
+}
---
+++ AtomEventStore/DataContractContentSerializer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+namespace Grean.AtomEventStore
+{
+    public class DataContractContentSerializer : IContentSerializer
+    {
+        public void Serialize(XmlWriter xmlWriter, object value)
+        {
+            if (xmlWriter == null)
+                throw new ArgumentNullException("xmlWriter");
+            if (value == null)
+                throw new ArgumentNullException("value");
+        }
+
+        public XmlAtomContent Deserialize(XmlReader xmlReader)
+        {
+            throw new ArgumentNullException("xmlReader");
+        }
+    }
+}
