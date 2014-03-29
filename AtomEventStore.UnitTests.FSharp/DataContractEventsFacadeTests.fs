e5a7d477c27af544bb0533d594c9e4b9a16ff677
Mark Seemann <mark@ploeh.dk>
2014-03-29 10:47:17 +0100

    Demonstrated writing and reading multiple events
    using DataContractContentSerializer.

--- AtomEventStore.UnitTests.FSharp/DataContractEventsFacadeTests.fs
+++ AtomEventStore.UnitTests.FSharp/DataContractEventsFacadeTests.fs
@@ -3,6 +3,7 @@
 open Grean.AtomEventStore
 open Grean.AtomEventStore.UnitTests.FSharp.DataContractRecords
 open Grean.AtomEventStore.UnitTests.FSharp.TestDsl
+open Ploeh.AutoFixture
 open Xunit.Extensions
 
 [<Theory; InMemoryDataContractConventions>]
@@ -16,3 +17,17 @@ let SutCorrectlyRoundTripsASingleElement
 
     Verify <@ actual.Length = 1 @>
     Verify <@ actual |> Seq.exactlyOne = tef @>
+
+[<Theory; InMemoryDataContractConventions>]
+let SutCorrectlyRoundTripsMultipleElements
+    (writer : AtomEventObserver<TestEventF>)
+    (reader : FifoEvents<TestEventF>)
+    (g : Generator<TestEventF>) =
+
+    let tefs = g |> Seq.take 3 |> Seq.toList
+
+    tefs |> List.iter (fun tef -> writer.AppendAsync(tef).Wait())
+    let actual = reader |> Seq.toList
+
+    let expected = tefs
+    Verify <@ expected = actual @>
