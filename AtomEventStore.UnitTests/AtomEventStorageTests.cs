263f6ce76cb52fab2a65702d3488a49cde181bdd
Mark Seemann <mark@ploeh.dk>
2014-03-31 13:39:52 +0200

    Introduced AtomEventStorage.CreateNewFeed helper
    method, because I've noticed that the same code appears in three different
    places across two different libraries.

---
+++ AtomEventStore.UnitTests/AtomEventStorageTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+using Xunit;
+using Xunit.Extensions;
+
+namespace Grean.AtomEventStore.UnitTests
+{
+    public class AtomEventStorageTests
+    {
+        [Theory, AutoAtomData]
+        public void CreateNewFeedReturnsCorrectResult(
+            Guid id,
+            IContentSerializer dummySerializer)
+        {
+            var href = new Uri(id.ToString(), UriKind.Relative);
+
+            XmlReader actual = AtomEventStorage.CreateNewFeed(href);
+
+            var expected = XDocument.Parse(
+                new AtomFeed(
+                    id,
+                    "Index of event stream " + id,
+                    DateTimeOffset.Now,
+                    new AtomAuthor("Grean"),
+                    Enumerable.Empty<AtomEntry>(),
+                    new[]
+                    {
+                        AtomLink.CreateSelfLink(href)
+                    })
+                .ToXmlString(dummySerializer));
+            Assert.Equal(expected, XDocument.Load(actual), new XNodeEqualityComparer());
+        }
+    }
+}
---
+++ AtomEventStore/AtomEventStorage.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Grean.AtomEventStore
+{
+    public static class AtomEventStorage
+    {
+
+        public static XmlReader CreateNewFeed(Uri href)
+        {
+            var id = GetIdFromHref(href);
+            var xml = new AtomFeed(
+                id,
+                "Index of event stream " + id,
+                DateTimeOffset.Now,
+                new AtomAuthor("Grean"),
+                Enumerable.Empty<AtomEntry>(),
+                new[]
+                {
+                    AtomLink.CreateSelfLink(href)
+                })
+                .ToXmlString((IContentSerializer)null);
+            var sr = new StringReader(xml);
+            return XmlReader.Create(sr);
+        }
+
+        private static Guid GetIdFromHref(Uri href)
+        {
+            return new Guid(href.ToString());
+        }
+    }
+}
