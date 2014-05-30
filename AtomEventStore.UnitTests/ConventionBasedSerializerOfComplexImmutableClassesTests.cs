fb0aa50deb0e12333a40443c869d0f1f8bb06f83
Mark Seemann <mark@ploeh.dk>
2014-05-30 08:14:43 +0200

    Moved a test case to the new test class,
    although the test still needs to be refactored to shift the focus to the
    new SUT.

--- AtomEventStore.UnitTests/ConventionBasedSerializerOfComplexImmutableClassesTests.cs
+++ AtomEventStore.UnitTests/ConventionBasedSerializerOfComplexImmutableClassesTests.cs
@@ -3,10 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
+using Xunit;
+using Xunit.Extensions;
 
 namespace Grean.AtomEventStore.UnitTests
 {
     public class ConventionBasedSerializerOfComplexImmutableClassesTests
     {
+        [Theory, AutoAtomData]
+        public void SutCanSerializeNestedItem(
+            XmlAtomContent seed,
+            Envelope<TestEventX> env)
+        {
+            var sut = seed.WithItem(env);
+
+            var actual = sut.ToXmlString(
+                new ConventionBasedSerializerOfComplexImmutableClasses());
+
+            var expected = XDocument.Parse(
+                "<content type=\"application/xml\" xmlns=\"http://www.w3.org/2005/Atom\">" +
+                "  <envelope xmlns=\"urn:grean:atom-event-store:unit-tests\">" +
+                "    <id>urn:uuid:" + env.Id + "</id>" +
+                "    <item>" +
+                "      <test-event-x>" +
+                "        <number>" + env.Item.Number + "</number>" +
+                "        <text>" + env.Item.Text + "</text>" +
+                "      </test-event-x>" +
+                "    </item>" +
+                "  </envelope>" +
+                "</content>");
+            Assert.Equal(expected, XDocument.Parse(actual), new XNodeEqualityComparer());
+        }
     }
 }
--- AtomEventStore.UnitTests/XmlAtomContentTests.cs
+++ AtomEventStore.UnitTests/XmlAtomContentTests.cs
@@ -145,31 +145,6 @@ namespace Grean.AtomEventStore.UnitTests
         }
 
         [Theory, AutoAtomData]
-        public void SutCanSerializeNestedItem(
-            XmlAtomContent seed,
-            Envelope<TestEventX> env)
-        {
-            var sut = seed.WithItem(env);
-
-            var actual = sut.ToXmlString(
-                new ConventionBasedSerializerOfComplexImmutableClasses());
-
-            var expected = XDocument.Parse(
-                "<content type=\"application/xml\" xmlns=\"http://www.w3.org/2005/Atom\">" +
-                "  <envelope xmlns=\"urn:grean:atom-event-store:unit-tests\">" +
-                "    <id>urn:uuid:" + env.Id + "</id>" +
-                "    <item>" +
-                "      <test-event-x>" +
-                "        <number>" + env.Item.Number + "</number>" +
-                "        <text>" + env.Item.Text + "</text>" +
-                "      </test-event-x>" +
-                "    </item>" +
-                "  </envelope>" +
-                "</content>");
-            Assert.Equal(expected, XDocument.Parse(actual), new XNodeEqualityComparer());
-        }
-
-        [Theory, AutoAtomData]
         public void SutCanRoundTripNestedItem(
             XmlAtomContent seed,
             Envelope<TestEventY> env)
