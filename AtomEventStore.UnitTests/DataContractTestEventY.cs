d8092ac17d8d0d02450d310b520a4e3e62f729fd
Mark Seemann <mark@ploeh.dk>
2014-06-16 15:55:51 +0200

    Added a DataContractTestEventY test event,
    although it's not yet used. It will replace TestEventY when testing
    certain polymorphic behaviour not supported by the XML serializer.

---
+++ AtomEventStore.UnitTests/DataContractTestEventY.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grean.AtomEventStore.UnitTests
+{
+    [DataContract(Name = "test-event-y", Namespace = "http://grean.rocks/dc")]
+    public class DataContractTestEventY
+    {
+        [DataMember(Name = "number")]
+        public long Number { get; set; }
+
+        [DataMember(Name = "text")]
+        public bool IsTrue { get; set; }
+    }
+}
