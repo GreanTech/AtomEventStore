6b03b39bc4edb7eb8bea2ddba66e92fcf106ed96
Mark Seemann <mark@ploeh.dk>
2014-06-16 09:43:29 +0200

    Refactored a test of AtomEventStream to use
    XmlContentSerializer, in order to decouple from the legacy serializer.
    This particular test case is not concerned with the actual serialization
    format, but rather that an Atom Entry is being correctly persisted.

--- AtomEventStore.UnitTests/AtomEventStreamTests.cs
+++ AtomEventStore.UnitTests/AtomEventStreamTests.cs
@@ -209,9 +209,9 @@ namespace Grean.AtomEventStore.UnitTests
         [Theory, AutoAtomData]
         public void AppendAsyncMoreThanPageSizeEventsAddsLinkToPreviousPageToIndex(
             [Frozen(As = typeof(IAtomEventStorage))]AtomEventsInMemory storage,
-            AtomFeedParser<ConventionBasedSerializerOfComplexImmutableClasses> parser,
-            AtomEventStream<TestEventX> sut,
-            Generator<TestEventX> eventGenerator)
+            AtomFeedParser<XmlContentSerializer> parser,
+            AtomEventStream<XmlAttributedTestEventX> sut,
+            Generator<XmlAttributedTestEventX> eventGenerator)
         {
             var before = DateTimeOffset.Now;
             var events = eventGenerator.Take(sut.PageSize + 1).ToList();
