c5cbcc6bc037b24f52638dd406f26bc3b3c793d1
Mark Seemann <mark@ploeh.dk>
2014-06-16 15:47:02 +0200

    Refactored a test to use XML-attributed events,
    instead of convention-based events, in order to decrease the implicit
    coupling to ConventionBasedSerializerOfComplexImmutableClasses.

--- AtomEventStore.UnitTests/AtomEventStreamTests.cs
+++ AtomEventStore.UnitTests/AtomEventStreamTests.cs
@@ -319,8 +319,8 @@ namespace Grean.AtomEventStore.UnitTests
         [Theory, AutoAtomData]
         public void AppendAsyncMoreThanTwicePageSizeEventsCreatesThreeFeedPages(
             [Frozen(As = typeof(IAtomEventStorage))]AtomEventsInMemory storage,
-            AtomEventStream<TestEventX> sut,
-            Generator<TestEventX> eventGenerator)
+            AtomEventStream<XmlAttributedTestEventX> sut,
+            Generator<XmlAttributedTestEventX> eventGenerator)
         {
             var events = eventGenerator.Take(sut.PageSize * 2 + 1).ToList();
             events.ForEach(e => sut.AppendAsync(e).Wait());
