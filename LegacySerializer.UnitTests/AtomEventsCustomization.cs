76fba24c998d9c99f17e307b346cf3d393f72994
Mark Seemann <mark@ploeh.dk>
2014-07-17 21:11:21 +0200

    Removed PageSizeCustomization from legacy tests,
    because the legacy Serializer has no pageSize constructor argument.

--- LegacySerializer.UnitTests/AtomEventsCustomization.cs
+++ LegacySerializer.UnitTests/AtomEventsCustomization.cs
@@ -14,35 +14,10 @@ namespace Grean.AtomEventStore.LegacySerializer.UnitTests
     {
         public AtomEventsCustomization()
             : base(
-                new PageSizeCustomization(),
                 new DirectoryCustomization(),
                 new StreamCustomization(),
                 new AutoMoqCustomization())
         {
-        }
-
-        private class PageSizeCustomization : ICustomization
-        {
-            public void Customize(IFixture fixture)
-            {
-                fixture.Customizations.Add(new PageSizeRelay());
-            }
-
-            private class PageSizeRelay : ISpecimenBuilder
-            {
-                private readonly Random r = new Random();
-
-                public object Create(object request, ISpecimenContext context)
-                {
-                    var pi = request as ParameterInfo;
-                    if (pi == null ||
-                        pi.ParameterType != typeof(int) ||
-                        pi.Name != "pageSize")
-                        return new NoSpecimen(request);
-
-                    return this.r.Next(2, 17);
-                }
-            }
         }
 
         private class DirectoryCustomization : ICustomization
