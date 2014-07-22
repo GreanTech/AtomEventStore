cc11a60b48937ddc27391e32073afa9811f239c1
Mark Seemann <mark@ploeh.dk>
2014-07-22 11:22:56 +0200

    Moved legacy serializer from core library
    to legacy serializer library.

--- LegacySerializer/ConventionBasedSerializerOfComplexImmutableClasses.cs
+++ LegacySerializer/ConventionBasedSerializerOfComplexImmutableClasses.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 using System.Xml;
 using System.Xml.Linq;
 
-namespace Grean.AtomEventStore
+namespace Grean.AtomEventStore.LegacySerializer
 {
     public class ConventionBasedSerializerOfComplexImmutableClasses : IContentSerializer
     {
--- LegacySerializer/XmlCasedName.cs
+++ LegacySerializer/XmlCasedName.cs
@@ -7,7 +7,7 @@ using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Grean.AtomEventStore
+namespace Grean.AtomEventStore.LegacySerializer
 {
     internal class XmlCasedName
     {
