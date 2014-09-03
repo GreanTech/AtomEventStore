2436f5c66b898d6a03ac6e2e45b6109d7b64bbf9
Mark Seemann <mark@ploeh.dk>
2014-09-03 08:57:27 +0200

    Documented IContentSerializer.Serialize.

--- AtomEventStore/IContentSerializer.cs
+++ AtomEventStore/IContentSerializer.cs
@@ -18,6 +18,29 @@ namespace Grean.AtomEventStore
     /// <seealso cref="DataContractContentSerializer" />
     public interface IContentSerializer
     {
+        /// <summary>
+        /// Serializes an object to XML and writes it to an
+        /// <see cref="XmlWriter" />.
+        /// </summary>
+        /// <param name="xmlWriter">
+        /// The <see cref="XmlWriter" /> to which the serialized object should
+        /// be written.
+        /// </param>
+        /// <param name="value">
+        /// The object to serialize to XML.
+        /// </param>
+        /// <remarks>
+        /// <para>
+        /// <strong>Note to implementers:</strong>
+        /// </para>
+        /// <para>
+        /// The XML written by this method must be readable by the
+        /// complementary implementation of
+        /// <see cref="Deserialize(XmlReader)" />. In other words, the
+        /// implementation must be able to round-trip an object to and from
+        /// XML.
+        /// </para>
+        /// </remarks>
         void Serialize(XmlWriter xmlWriter, object value);
 
         XmlAtomContent Deserialize(XmlReader xmlReader);
