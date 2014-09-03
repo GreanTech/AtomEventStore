c65fae6f9a362a6df62d9dbb1e700f4c217e2da4
Mark Seemann <mark@ploeh.dk>
2014-09-03 07:46:11 +0200

    Added a bit of documentation to IContentSerializer

--- AtomEventStore/IContentSerializer.cs
+++ AtomEventStore/IContentSerializer.cs
@@ -3,6 +3,19 @@ using System.Xml;
 
 namespace Grean.AtomEventStore
 {
+    /// <summary>
+    /// Serializes and deserializes object to XML.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// This interface is used by AtomEventStore to serialize the contents of
+    /// <see cref="XmlAtomContent" /> instances to and from XML. This enables
+    /// users of AtomEventStore to pick the serialization mechanism that works
+    /// best for their needs.
+    /// </para>
+    /// </remarks>
+    /// <seealso cref="XmlContentSerializer" />
+    /// <seealso cref="DataContractContentSerializer" />
     public interface IContentSerializer
     {
         void Serialize(XmlWriter xmlWriter, object value);
