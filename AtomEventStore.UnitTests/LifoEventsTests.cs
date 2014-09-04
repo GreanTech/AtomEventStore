7f67ec0624290f0d05443a734ec82752e2c97ed0
Nikos Baxevanis <nikos.baxevanis@gmail.com>
2014-09-04 23:25:21 +0300

    Returned empty iterator when no events are in store.

--- AtomEventStore.UnitTests/LifoEventsTests.cs
+++ AtomEventStore.UnitTests/LifoEventsTests.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 using Xunit;
 using Xunit.Extensions;
+using Ploeh.AutoFixture.Idioms;
+using Ploeh.AutoFixture.Xunit;
 
 namespace Grean.AtomEventStore.UnitTests
 {
@@ -14,6 +16,22 @@ namespace Grean.AtomEventStore.UnitTests
         public void SutIsEnumerable(LifoEvents<XmlAttributedTestEventX> sut)
         {
             Assert.IsAssignableFrom<IEnumerable<XmlAttributedTestEventX>>(sut);
+        }
+
+        [Theory, AutoAtomData]
+        public void PropertiesAreCorrectlyInitialized(
+            ConstructorInitializedMemberAssertion assertion)
+        {
+            assertion.Verify(typeof(LifoEvents<XmlAttributedTestEventX>));
+        }
+
+        [Theory, AutoAtomData]
+        public void SutIsInitiallyEmpty(
+            [Frozen(As = typeof(IAtomEventStorage))]AtomEventsInMemory dummyInjectedIntoSut,
+            LifoEvents<XmlAttributedTestEventX> sut)
+        {
+            Assert.False(sut.Any(), "Intial event stream should be empty.");
+            Assert.Empty(sut);
         }
     }
 }
