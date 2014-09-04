c92d25497795201e1ec32faf3eb0b15c2930589f
Mark Seemann <mark@ploeh.dk>
2014-09-04 21:14:22 +0200

    Documented AtomEventsInMemory.Dispose methods.

--- AtomEventStore/AtomEventsInMemory.cs
+++ AtomEventStore/AtomEventsInMemory.cs
@@ -233,12 +233,24 @@ namespace Grean.AtomEventStore
             return this.GetEnumerator();
         }
 
+        /// <summary>
+        /// Performs application-defined tasks associated with freeing,
+        /// releasing, or resetting unmanaged resources.
+        /// </summary>
         public void Dispose()
         {
             this.Dispose(true);
             GC.SuppressFinalize(this);
         }
 
+        /// <summary>
+        /// Releases unmanaged and - optionally - managed resources.
+        /// </summary>
+        /// <param name="disposing">
+        /// <see langword="true" /> to release both managed and unmanaged
+        /// resources; <see langword="false" /> to release only unmanaged
+        /// resources.
+        /// </param>
         protected virtual void Dispose(bool disposing)
         {
             if (disposing)
