d4f75f517e7fde0445657ebb9ba8fa5a02af0af8
Nikos Baxevanis <nikos.baxevanis@gmail.com>
2014-09-04 23:23:10 +0300

    Introduced LifoEvents<T> supplying Last In, First Out enumeration of events in the underlying store.

---
+++ AtomEventStore.UnitTests/LifoEventsTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Extensions;
+
+namespace Grean.AtomEventStore.UnitTests
+{
+    public class LifoEventsTests
+    {
+        [Theory, AutoAtomData]
+        public void SutIsEnumerable(LifoEvents<XmlAttributedTestEventX> sut)
+        {
+            Assert.IsAssignableFrom<IEnumerable<XmlAttributedTestEventX>>(sut);
+        }
+    }
+}
---
+++ AtomEventStore/LifoEvents.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grean.AtomEventStore
+{
+    public class LifoEvents<T> : IEnumerable<T>
+    {
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
