0a8602e3f4f1aceb15bf0752f4d669688e71bbc9
Mark Seemann <mark@ploeh.dk>
2014-09-11 21:18:54 +0200

    Documented AtomLink.IsLastLink.

--- AtomEventStore/AtomLink.cs
+++ AtomEventStore/AtomLink.cs
@@ -502,6 +502,19 @@ namespace Grean.AtomEventStore
             return new AtomLink("last", uri);
         }
 
+        /// <summary>
+        /// Gets a value indicating whether this instance is a last link.
+        /// </summary>
+        /// <value>
+        /// <see langword="true" /> if this instance is a last link;
+        /// otherwise, <see langword="false" />.
+        /// </value>
+        /// <remarks>
+        /// <para>
+        /// An <see cref="AtomLink" /> is considered a last link if its
+        /// <see cref="Rel" /> value is "last".
+        /// </para>
+        /// </remarks>
         public bool IsLastLink
         {
             get { return this.rel == "last"; }
