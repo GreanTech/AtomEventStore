361c40fc122b2da4151c793da14a0131c599d6a9
Mark Seemann <mark@ploeh.dk>
2014-09-12 07:28:57 +0200

    Documented XmlWritable.

--- AtomEventStore/XmlWritable.cs
+++ AtomEventStore/XmlWritable.cs
@@ -7,8 +7,26 @@ using System.Xml;
 
 namespace Grean.AtomEventStore
 {
+    /// <summary>
+    /// Extension methods related to <see cref="IXmlWritable" />.
+    /// </summary>
     public static class XmlWritable
     {
+        /// <summary>
+        /// Converts an <see cref="IXmlWritable" /> to an XML string.
+        /// </summary>
+        /// <param name="xmlWritable">
+        /// The object that can be converted to XML.
+        /// </param>
+        /// <param name="serializer">
+        /// A serializer that can serialize custom content, in case the object
+        /// contains custom content.
+        /// </param>
+        /// <returns>
+        /// A string of characters containing XML corresponding to the data in
+        /// the object.
+        /// </returns>
+        /// <seealso cref="ToXmlString(IXmlWritable, IContentSerializer, XmlWriterSettings)" />
         public static string ToXmlString(
             this IXmlWritable xmlWritable,
             IContentSerializer serializer)
@@ -16,6 +34,23 @@ namespace Grean.AtomEventStore
             return xmlWritable.ToXmlString(serializer, new XmlWriterSettings());
         }
 
+        /// <summary>
+        /// Converts an <see cref="IXmlWritable" /> to an XML string.
+        /// </summary>
+        /// <param name="xmlWritable">
+        /// The object that can be converted to XML.
+        /// </param>
+        /// <param name="serializer">
+        /// A serializer that can serialize custom content, in case the object
+        /// contains custom content.
+        /// </param>
+        /// <param name="settings">
+        /// Settings that control how the XML is formatted.
+        /// </param>
+        /// <returns>
+        /// A string of characters containing XML corresponding to the data in
+        /// the object.
+        /// </returns>
         public static string ToXmlString(
             this IXmlWritable xmlWritable,
             IContentSerializer serializer,
