ad4144d16abbb85185adb1edced3e563b4f2443e
Mark Seemann <mark@ploeh.dk>
2014-09-12 16:46:48 +0200

    Corrected a wrong XML comment reference.

--- AtomEventStore/LifoEvents.cs
+++ AtomEventStore/LifoEvents.cs
@@ -247,7 +247,7 @@ namespace Grean.AtomEventStore
         /// </para>
         /// </remarks>
         /// <seealso cref="Storage" />
-        /// <seealso cref="AppendAsync(T)" />
+        /// <seealso cref="AtomEventObserver{T}.AppendAsync(T)" />
         /// <seealso cref="LifoEvents{T}(UuidIri, IAtomEventStorage, IContentSerializer)" />
         public IContentSerializer Serializer
         {
