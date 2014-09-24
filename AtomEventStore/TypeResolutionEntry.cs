98cd69ec832b2bde18885fb982fd5a12be9c08d8
Nikos Baxevanis <nikos.baxevanis@gmail.com>
2014-09-24 21:54:33 +0300

    Added XmlNamespace property to TypeResolutionEntry.

--- AtomEventStore/TypeResolutionEntry.cs
+++ AtomEventStore/TypeResolutionEntry.cs
@@ -8,5 +8,17 @@ namespace Grean.AtomEventStore
 {
     public class TypeResolutionEntry
     {
+        private readonly string xmlNamespace;
+
+        public TypeResolutionEntry(
+            string xmlNamespace)
+        {
+            this.xmlNamespace = xmlNamespace;
+        }
+
+        public string XmlNamespace
+        {
+            get { return this.xmlNamespace; }
+        }
     }
 }
