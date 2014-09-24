b938073888906ed20c9b9d1d04fcac14516dd48f
Nikos Baxevanis <nikos.baxevanis@gmail.com>
2014-09-24 21:52:15 +0300

    Verified TypeResolutionEntry Properties.

--- AtomEventStore.UnitTests/TypeResolutionEntryTests.cs
+++ AtomEventStore.UnitTests/TypeResolutionEntryTests.cs
@@ -1,12 +1,20 @@
-﻿using System;
+﻿using Ploeh.AutoFixture.Idioms;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit.Extensions;
 
 namespace Grean.AtomEventStore.UnitTests
 {
     public class TypeResolutionEntryTests
     {
+        [Theory, AutoAtomData]
+        public void VerifyConstructorInitializedProperties(
+            ConstructorInitializedMemberAssertion assertion)
+        {
+            assertion.Verify(typeof(TypeResolutionEntry).GetProperties());
+        }
     }
 }
