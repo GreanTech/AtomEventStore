9cf43a7d63a96ab92c0aa206e86561f39d2eb167
Nikos Baxevanis <nikos.baxevanis@gmail.com>
2014-09-24 21:56:41 +0300

    Added LocalName property to TypeResolutionEntry.

--- AtomEventStore/TypeResolutionEntry.cs
+++ AtomEventStore/TypeResolutionEntry.cs
@@ -9,19 +9,27 @@ namespace Grean.AtomEventStore
     public class TypeResolutionEntry
     {
         private readonly string xmlNamespace;
+        private readonly string localName;
 
         public TypeResolutionEntry(
-            string xmlNamespace)
+            string xmlNamespace,
+            string localName)
         {
             if (xmlNamespace == null)
                 throw new ArgumentNullException("xmlNamespace");
 
             this.xmlNamespace = xmlNamespace;
+            this.localName = localName;
         }
 
         public string XmlNamespace
         {
             get { return this.xmlNamespace; }
+        }
+
+        public string LocalName
+        {
+            get { return this.localName; }
         }
     }
 }
