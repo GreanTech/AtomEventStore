f35de83972641487059a15b698fc9d7d5d403d73
Nikos Baxevanis <nikos.baxevanis@gmail.com>
2014-09-25 13:16:04 +0300

    Documented TypeResolutionEntry constructor.

--- AtomEventStore/TypeResolutionEntry.cs
+++ AtomEventStore/TypeResolutionEntry.cs
@@ -21,6 +21,29 @@ namespace Grean.AtomEventStore
         private readonly string localName;
         private readonly Type resolution;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TypeResolutionEntry"/>
+        /// class.
+        /// </summary>
+        /// <param name="xmlNamespace">The XML namespace of the XML name.
+        /// </param>
+        /// <param name="localName">The local name of the XML name.</param>
+        /// <param name="resolution">The <see cref="Type"/> resolved by the
+        /// XML namespace and the local name of the XML name.
+        /// </param>
+        /// <remarks>
+        /// <para>
+        /// The constructor arguments are subsequently available on the object
+        /// as properties.
+        /// </para>
+        /// </remarks>
+        /// <exception cref="System.ArgumentNullException">
+        /// <paramref name="xmlNamespace" />
+        /// or
+        /// <paramref name="localName" />
+        /// or
+        /// <paramref name="resolution" /> is <see langword="null" />.
+        /// </exception>
         public TypeResolutionEntry(
             string xmlNamespace,
             string localName,
